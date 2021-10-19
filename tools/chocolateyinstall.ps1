e25057d9c207342386cc6b6a432860f969f31eec
Oldes <oldes.huhuman@gmail.com>
2021-10-19 23:27:02 +0200

    v3.6.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Oldes/Rebol3/releases/download/3.5.6/rebol3-bulk-x86-vs.exe'
-$url64      = 'https://github.com/Oldes/Rebol3/releases/download/3.5.6/rebol3-bulk-x64-vs.exe'
+$url        = 'https://github.com/Oldes/Rebol3/releases/download/3.6.0/rebol3-bulk-x86-vs.exe'
+$url64      = 'https://github.com/Oldes/Rebol3/releases/download/3.6.0/rebol3-bulk-x64-vs.exe'
 
 $packageArgs = @{
   packageName   = 'rebol3'
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'rebol3*'
 
-  checksum      = 'FC0D3B75E284393A5219709E0A295BFC6DD0001E866382ECA2FF8E4B1B51B1F2'
+  checksum      = 'AADBACB7FD2A1476D6320DD18C80D7137A16B7F678448E2EBCF33985DCD77A47'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '85BB1EF58683EE3E21C4A6DD229B612FC2109E5409462AA5EA8F82C811300364'
+  checksum64    = '7641E85592E60AD20BB231B3E500375557210FB53EAB81BF34A414C659919766'
   checksumType64= 'sha256' #default is checksumType
 
 }
